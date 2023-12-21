d5e1717513ef5de9b32c9223ea2cf409af592ac4
Claudio V. Carta <claudio.vittorio.carta@gmail.com>
2023-12-21 12:22:19 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+       programma(1, 2);
+
+    }
+    public static void programma(int a, int b) {
+
+        int c = (a+3) * (b+3);
+        System.out.println("il risultato del programma è: " + c);
+    }
+}
